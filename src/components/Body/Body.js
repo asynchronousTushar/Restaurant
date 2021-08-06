c15d313ebe8495bacc5c5cc11965f8538584b94e
Tushar Biswas <tusarbiswas888@gmail.com>
2021-08-06 16:16:03 +0600

    Folder structure creation

---
+++ src/components/Body/Body.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Body = () => {
+    return (
+        <div>
+            efe
+        </div>
+    );
+}
+
+export default Body;
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>eq
+            
+        </div>
+    );
+}
+
+export default Footer;
